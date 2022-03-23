6fcd8f16d33e6327bfd79476dc05b3f937340c44
CarolBreitenbach <carolinebreitenbach@gmail.com>
2022-03-23 12:09:21 -0300

    Inserção de usuário com POST

--- src/main/java/com/carolbreitenbach/workshopmongo/services/UserService.java
+++ src/main/java/com/carolbreitenbach/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carolbreitenbach.workshopmongo.domain.User;
+import com.carolbreitenbach.workshopmongo.dto.UserDTO;
 import com.carolbreitenbach.workshopmongo.repository.UserRepository;
 import com.carolbreitenbach.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,14 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
+	
 }
