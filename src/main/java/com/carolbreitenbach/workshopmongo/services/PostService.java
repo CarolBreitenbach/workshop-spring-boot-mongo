be8dd47fe1fe5675d1502deb0f8e4667c7ca02ce
CarolBreitenbach <carolinebreitenbach@gmail.com>
2022-03-23 14:44:49 -0300

    Obtendo um post por id

---
+++ src/main/java/com/carolbreitenbach/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.carolbreitenbach.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carolbreitenbach.workshopmongo.domain.Post;
+import com.carolbreitenbach.workshopmongo.repository.PostRepository;
+import com.carolbreitenbach.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+
+}
